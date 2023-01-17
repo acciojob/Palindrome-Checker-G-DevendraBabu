a9d892be3654070f6b18db67a40b6d1958f02719
G-DevendraBabu <116060950+G-DevendraBabu@users.noreply.github.com>
2023-01-17 22:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function palindrome(str)
 {
 	const len = str.length;
 
-    // loop through half of the string
+    
     for (let i = 0; i < len / 2; i++) {
 
-        // check if first and last string are same
-        if (str[i] !== str[len - 1 - i]) {
+       
+        if (str[i].toLowerCase() !== str[len - 1 - i].toLowerCase()) {
             return 'false';
         }
     }
