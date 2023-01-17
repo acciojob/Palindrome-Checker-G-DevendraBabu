d37ffefdf164e7fec3aedcc1c6afca9aea0fe3fc
G-DevendraBabu <116060950+G-DevendraBabu@users.noreply.github.com>
2023-01-17 22:18:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(str)
+{
+	if(!str || str.length < 2 || 
+                typeof str!== 'string') { 
+            return 'Not valid'; 
+        } 
+          
+        
+        const revArray = []; 
+        const length = str.length - 1; 
+          
+        
+        for(let i = length; i >= 0; i--) { 
+            revArray.push(str[i]); 
+        } 
+          
+      
+        return revArray.join(''); 
 }
 module.exports = palindrome
